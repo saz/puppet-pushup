28f30414fe266d69d264a987711cc1c4a92d521b
Saz <me@saz.sh>
2011-12-13 18:37:42 +0100

    add variables for service configuration, service monitoring and firewalling

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,6 +11,21 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class pushup ($bindAddress = $::ipaddress, $adminBindAddress = '127.0.0.1', $adminPassword = 'password', $host_name = $::fqdn) {
+class pushup (
+    $ensure = present,
+    $bindAddress = $::ipaddress,
+    $adminBindAddress = '127.0.0.1',
+    $adminPassword = 'password',
+    $host_name = $::fqdn,
+    $service_ensure = running,
+    $service_onboot = true,
+    $service_disable = false,
+    $monitor = false,
+    $monitor_tool = false,
+    $firewall = false,
+    $firewall_tool = false,
+    $firewall_src = false,
+    $firewall_dst = $::ipaddress
+) {
     include pushup::params, pushup::install, pushup::config, pushup::service
 }
