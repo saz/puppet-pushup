4759bde2f2bccbb42ac54057ca2b63b017ccfa90
Saz <me@saz.sh>
2011-12-13 18:36:46 +0100

    make it configurable, if service should be stopped/running, enabled/disabled at boot

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,7 +1,19 @@
 class pushup::service {
     service { $pushup::params::service_name:
-        ensure     => running,
-        enable     => true,
+        ensure     => $pushup::service_disable ? {
+            true  => stopped,
+            false => $pushup::ensure ? {
+                absent  => stopped,
+                present => $pushup::service_ensure,
+            },
+        },
+        enable     => $pushup::service_disable ? {
+            true  => false,
+            false => $pushup::ensure ? {
+                absent  => false,
+                present => $pushup::service_onboot,
+            },
+        },
         hasstatus  => false,
         hasrestart => true,
         require    => Class['pushup::config'],
